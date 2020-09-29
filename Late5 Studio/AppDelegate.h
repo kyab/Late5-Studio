a0b073bf58b49c4778e1c0cfd75e90ca77dc58db
kyab <kyossi212@gmail.com>
2020-09-29 18:59:21 +0900

    Initial Commit

---
+++ Late5 Studio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Late5 Studio
+//
+//  Created by kyab on 2020/09/29.
+//  Copyright © 2020 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Late5 Studio/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  Late5 Studio
+//
+//  Created by kyab on 2020/09/29.
+//  Copyright © 2020 kyab. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
